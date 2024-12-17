3b4538eb77fdf234f4a2f46372fcbc9bfce02db2
souramam1 <maia.ramambason20@imperial.ac.uk>
2024-12-17 15:11:41 +0000

    notes on main to explain opencv 01

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import cv2
-from PIL import Image
+import cv2             #open cv lib
+from PIL import Image  # Pillow library image class for processing of image boxes here
 
-from util import get_limits
+from util import get_limits # import the get limits function from util file - this allows for HSV thresholds to be calculated dynamically
 
 
 yellow = [0, 255, 255]  # yellow in BGR 
 red = [0,0,255] # red in BGR
-blue = [255,0,0]
-cap = cv2.VideoCapture(0) #the index of the webcam you want to connect to
+blue = [255,0,0] # blue in BGR
+cap = cv2.VideoCapture(0) #the index of the webcam you want to connect to (if attaching an overhead will be 1,2 etc.)
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read()  # ret is a Boolean that returns True or False (i.e false would be no frames are captured) , frame is the actual frame
 
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #converting from BGR colourspace to HSV (hue, saturation, value) - better suited to object identification, more similar to how we see objects.
 
-    lowerLimit, upperLimit = get_limits(color=yellow)
+    lowerLimit, upperLimit = get_limits(color=blue) #
 
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
