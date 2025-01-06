38f5c6786e20f3ae1a111ddcc240bf1e26279313
souramam1 <maia.ramambason20@imperial.ac.uk>
2025-01-06 17:39:17 +0000

    pi camera opencv tutorial

---
+++ opencv_pi_tuto/V1R3.py
@@ -0,0 +1,16 @@
+import cv2
+from picamera2 import Picamera2
+import time
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(raw{"size":(1640,1232)},main={"format":'RGB888', "size": (640,480)}))
+picam2.start()
+time.sleep(2)
+
+img = picam2.capture_array()
+
+cv2.imshow("Output",img)
+cv2.waitKey(0)
+
+picam2.stop()
+picam2.close()
