4cb128817e89bbf9799e8f8ab679dfa507f63b93
souramam1 <maia.ramambason20@imperial.ac.uk>
2025-01-07 12:23:05 +0000

    error handling added

--- V1R3.py
+++ V1R3.py
@@ -4,14 +4,28 @@ import time
 
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(raw={"size":(1640,1232)},main={"format":'RGB888', "size": (640,480)}))
-picam2.start()
-time.sleep(2)
+# picam2.start()
+# time.sleep(2)
 
-img = picam2.capture_array()
 
-cv2.imwrite("output.jpg", img)
-cv2.imshow("Output",img)
-cv2.waitKey(0)
+# img = picam2.capture_array()
 
-picam2.stop()
-picam2.close()
+# cv2.imwrite("output.jpg", img)
+# cv2.imshow("Output",img)
+# cv2.waitKey(0)
+
+# picam2.stop()
+# picam2.close()
+
+
+try:
+    picam2.start()
+    time.sleep(2)
+    img = picam2.capture_array()
+    cv2.imwrite("output.jpg", img)
+    #cv2.imshow("output",img)
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    picam2.stop()
+    picam2.close()
