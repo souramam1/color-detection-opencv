220cbd5689bfd6104b1f1efb222dae8fd805c37d
souramam1 <maia.ramambason20@imperial.ac.uk>
2025-01-16 16:20:51 +0000

    fixed the sdk issue with relative path

--- Image_Processing_Improvements/FIrestore_Tutorial/RPi_Firestore.py
+++ Image_Processing_Improvements/FIrestore_Tutorial/RPi_Firestore.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# VERY IMPORTANT - THE PATH MUST BE THE RELATIVE PATH OR IT CANNOT FIND THE SDK FILE
+cred = credentials.Certificate("Image_Processing_Improvements\Firestore_Tutorial\serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
