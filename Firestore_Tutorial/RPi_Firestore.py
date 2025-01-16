cb9b9b337fe399b3f2a576b078c07ece1532e239
souramam1 <maia.ramambason20@imperial.ac.uk>
2025-01-16 14:22:19 +0000

    firestore setting up collections

--- Firestore_Tutorial/RPi_Firestore.py
+++ Firestore_Tutorial/RPi_Firestore.py
@@ -5,3 +5,8 @@ from firebase_admin import firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
  
+######## in the tutorial he puts stuff in here to control gpio pins #######
+
+###########################################################################
+
+db = firestore.client()
