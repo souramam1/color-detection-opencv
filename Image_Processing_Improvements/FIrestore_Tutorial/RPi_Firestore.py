1d7b1cd190ef5d0fd595fc94fb3637d06a351d48
souramam1 <maia.ramambason20@imperial.ac.uk>
2025-01-16 16:12:40 +0000

    committing to try run it on laptop

---
+++ Image_Processing_Improvements/FIrestore_Tutorial/RPi_Firestore.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+db.collection('outputDevices').document('Camera').set(
+    {
+        'status' : False
+    }
+)
